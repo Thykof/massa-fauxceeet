8d0b2d44ebf6427fe9de6271a6253b10be70d778
Nathan Seva <thykof@protonmail.ch>
2024-05-01 12:59:47 -0500

    smart contract

--- smart-contract/assembly/contracts/main.ts
+++ smart-contract/assembly/contracts/main.ts
@@ -1,32 +1,97 @@
 // The entry file of your WebAssembly module.
-import { Context, generateEvent } from '@massalabs/massa-as-sdk';
-import { Args, stringToBytes } from '@massalabs/as-types';
+import {
+  Address,
+  balance,
+  Context,
+  getKeys,
+  setBytecode as _setBytecode,
+  Storage,
+  transferCoins,
+  generateEvent,
+  createEvent,
+} from '@massalabs/massa-as-sdk';
+import { Args, bytesToString, u8toByte } from '@massalabs/as-types';
+import {
+  onlyOwner,
+  ownerAddress,
+  setOwner,
+} from '@massalabs/sc-standards/assembly/contracts/utils/ownership';
+
+const historyKey = u8toByte(0);
 
 /**
  * This function is meant to be called only one time: when the contract is deployed.
  *
  * @param binaryArgs - Arguments serialized with Args
  */
-export function constructor(binaryArgs: StaticArray<u8>): StaticArray<u8> {
+export function constructor(_: StaticArray<u8>): StaticArray<u8> {
   // This line is important. It ensures that this function can't be called in the future.
   // If you remove this check, someone could call your constructor function and reset your smart contract.
   if (!Context.isDeployingContract()) {
     return [];
   }
-  const argsDeser = new Args(binaryArgs);
-  const name = argsDeser
-    .nextString()
-    .expect('Name argument is missing or invalid');
-  generateEvent(`Constructor called with name ${name}`);
+  setOwner(new Args().add(Context.caller()).serialize());
   return [];
 }
 
-/**
- * @param _ - not used
- * @returns the emitted event serialized in bytes
- */
-export function event(_: StaticArray<u8>): StaticArray<u8> {
-  const message = "I'm an event!";
-  generateEvent(message);
-  return stringToBytes(message);
+export function fauxceeet(_: StaticArray<u8>): void {
+  const caller = Context.caller().toString();
+  if (!Storage.has(historyKey)) {
+    Storage.set(historyKey, new Args().add([caller]).serialize());
+  } else {
+    const list = new Args(Storage.get(historyKey))
+      .nextStringArray()
+      .expect('list is invalid');
+    assert(!list.includes(caller), 'You already called this function');
+    list.push(caller);
+    Storage.set(historyKey, new Args().add(list).serialize());
+  }
+
+  transferCoins(new Address(caller), 500_000);
+}
+
+export function deposit(): void {
+  const amount = Context.transferredCoins();
+  generateEvent(createEvent('DepositEvent', [amount.toString()]));
+}
+
+// Admin
+
+export function resetHistory(_: StaticArray<u8>): void {
+  onlyOwner();
+  Storage.del(historyKey);
+}
+
+export function resetStorage(): void {
+  onlyOwner();
+  const keys = getKeys();
+  for (let i = 0; i < keys.length; i++) {
+    Storage.del(keys[i]);
+  }
+}
+
+export function selfDestruct(_: StaticArray<u8>): void {
+  onlyOwner();
+  // 1- empty the SC
+  let emptySc = new StaticArray<u8>(0);
+  _setBytecode(emptySc);
+
+  // 2- delete everything in Storage
+  let keys = getKeys();
+  for (let i = 0; i < keys.length; i++) {
+    Storage.del(keys[i]);
+  }
+
+  // 3- transfer back coins if any
+  let scBalance = balance();
+  // Balance will most likely be > 0 as we deleted some keys from the Storage
+  // but if there is nothing in the Storage, no need to call transferCoins
+  if (scBalance > 0) {
+    transferCoins(new Address(bytesToString(ownerAddress([]))), scBalance);
+  }
+}
+
+export function setBytecode(bytecode: StaticArray<u8>): void {
+  onlyOwner();
+  _setBytecode(bytecode);
 }
--- smart-contract/src/deploy.ts
+++ smart-contract/src/deploy.ts
@@ -13,7 +13,7 @@ const secretKey = getEnvVariable('WALLET_SECRET_KEY');
 // Define deployment parameters
 const chainId = CHAIN_ID.BuildNet; // Choose the chain ID corresponding to the network you want to deploy to
 const maxGas = MAX_GAS_DEPLOYMENT; // Gas for deployment Default is the maximum gas allowed for deployment
-const fees = 0n; // Fees to be paid for deployment. Default is 0
+const fees = 10_000_000n; // Fees to be paid for deployment. Default is 0
 const waitFirstEvent = true;
 
 // Create an account using the private key
@@ -36,7 +36,7 @@ const deployerAccount = await WalletClient.getAccountFromSecretKey(secretKey);
     [
       {
         data: getScByteCode('build', 'main.wasm'), // smart contract bytecode
-        coins: fromMAS(0.1), // coins for deployment
+        coins: fromMAS(10), // coins for deployment
         args: new Args().addString('Test'), // arguments for deployment
       } as ISCData,
       // Additional smart contracts can be added here for deployment
